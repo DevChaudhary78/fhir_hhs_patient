8b8c46b9689f6f2eb519c9cf08c9c6546d598388
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2024-01-24 13:04:01 -0500

    Added and styled pagination links

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,48 +1,172 @@
 <template>
   <main>
-    <div id="card">
-      <h3>John Doe</h3>
-      <p><span>Address: </span>100 Main St E</p>
-      <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
-      <a href="#">Details</a>
+    <form id="search">
+      <input type="text" placeholder="Search Patient">
+      <button type="submit">Search</button>
+    </form>
+    <div id="card_container">
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
+      <div id="card">
+        <h3>John Doe</h3>
+        <p><span>Address: </span>100 Main St E</p>
+        <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
+        <a href="#">Details</a>
+      </div>
     </div>
-    <div id="card">
-      <h3>John Doe</h3>
-      <p><span>Address: </span>100 Main St E</p>
-      <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
-      <a href="#">Details</a>
-    </div>
-    <div id="card">
-      <h3>John Doe</h3>
-      <p><span>Address: </span>100 Main St E</p>
-      <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
-      <a href="#">Details</a>
-    </div>
-    <div id="card">
-      <h3>John Doe</h3>
-      <p><span>Address: </span>100 Main St E</p>
-      <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
-      <a href="#">Details</a>
-    </div>
-    <div id="card">
-      <h3>John Doe</h3>
-      <p><span>Address: </span>100 Main St E</p>
-      <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
-      <a href="#">Details</a>
-    </div>
-    <div id="card">
-      <h3>John Doe</h3>
-      <p><span>Address: </span>100 Main St E</p>
-      <p><span>Contact: </span>chaudharyd@hhsc.ca</p>
-      <a href="#">Details</a>
-    </div>
+    <nav id="pagination">
+      <ul>
+        <a href="#">
+          <li>Prev</li>
+        </a>
+        <a href="#">
+          <li>1</li>
+        </a>
+        <a href="#">
+          <li>2</li>
+        </a>
+        <a href="#">
+          <li>3</li>
+        </a>
+        <a href="#">
+          <li>4</li>
+        </a>
+        <a href="#">
+          <li>Next</li>
+        </a>
+      </ul>
+    </nav>
   </main>
 </template>
 
 <style scoped>
 main {
-  height: 85vh;
   background: #fde4cf;
+  height: 85vh;
+}
+
+main #search {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5%;
+}
+
+main>#card_container {
+  height: 90%;
   display: grid;
   padding: 1rem;
   gap: 1rem;
@@ -57,6 +181,27 @@ main {
   align-items: center;
   justify-content: space-around;
   background: #ffcfd2;
+}
+
+nav ul {
+  display: flex;
+  margin: 0 auto;
+  width: 10%;
+  justify-content: space-around;
+}
+
+nav ul a {
+  text-decoration: none;
+  padding: 0.4rem;
+}
+
+nav ul a:hover {
+  background: #b9fbc0;
+  border-radius: 0.2rem;
+}
+
+nav ul a li {
+  list-style: none;
 }
 
 #card h3 {
