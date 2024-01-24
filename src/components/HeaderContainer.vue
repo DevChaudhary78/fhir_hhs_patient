1bc196da38bcd8a145905501c191a11184350f37
Dev Chaudhary <devchaudhary.dc84.dc@gmail.com>
2024-01-24 12:27:31 -0500

    Changed some styling for header

--- src/components/HeaderContainer.vue
+++ src/components/HeaderContainer.vue
@@ -22,7 +22,36 @@ header {
   height: 8%;
   display: flex;
   align-items: center;
-  justify-content: space-around;
-  background: bisque;
+  justify-content: space-between;
+  background: #fbf8cc;
+  padding: 1rem;
+}
+
+nav {
+  width: 20%;
+}
+
+nav>ul {
+  display: flex;
+  justify-content: center;
+}
+
+nav>ul>a {
+  text-decoration: none;
+  font-size: 1.3rem;
+  background: #ffcfd2;
+  padding: 0.3rem;
+  border-radius: 0.2rem;
+  color: #003049;
+  margin: 0 1rem;
+}
+
+nav>ul>a:hover {
+  scale: 1.05;
+}
+
+nav>ul>a>li {
+  text-decoration: none;
+  list-style: none;
 }
 </style>
